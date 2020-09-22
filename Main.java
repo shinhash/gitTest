48e0077ebc26eb4e941470e06346c6c2ed3bf6c9
hsshin <shinhash123@gmail.com>
2020-09-22 11:30:23 +0900

    4 commit

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main{
 	public static void main(String[] args){
 		System.out.println("Hell ro world");
 		System.out.println("Third Test hello");
+		System.out.println("4 Test hello");
 	}	
 }
